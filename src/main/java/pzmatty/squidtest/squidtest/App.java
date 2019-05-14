832636828d4cb32402527e56a3c1ecf3e61b7c1a
Matias <pzmatty@hotmail.com>
2019-05-13 21:58:34 -0300

    Add files via upload

---
+++ src/main/java/pzmatty/squidtest/squidtest/App.java
@@ -0,0 +1,14 @@
+package pzmatty.squidtest.squidtest;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        new App().run();
+    }
+    
+    public void run() {
+    	
+    }
+    
+}
